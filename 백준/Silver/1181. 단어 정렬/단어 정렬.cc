efaaa9f22fddb18837dcd1724880f7ea2aeaef69
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-09-24 12:23:45 +0900

    [Silver V] Title: 단어 정렬, Time: 16 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,29 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(string a, string b) {
-	if(a.length() == b.length()) {
-		return a < b;
-	}
-	return a.length() < b.length();
-}
+//bool compare(string a, string b) {
+//	if(a.length() == b.length()) {
+//		return a < b;
+//	}
+//	return a.length() < b.length();
+//}
 
 int main() {
-	
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	
 	int N;
-	string word[20001];
-	
 	cin >> N;
-	for(int i = 0; i < N; i++) {
-		cin >> word[i];
+	vector<string> word(N);
+	for(auto& e : word) {
+		cin >> e;
 	}
-	sort(word, word + N, compare);
-	for(int i = 0; i < N; i++) {
-		if(word[i] != word[i+1]) {
+	
+	sort(word.begin(), word.end(), [](string& a, string& b) {
+		if(a.size() == b.size()) {
+			return a < b;
+		}
+		return a.size() < b.size();
+	});
+	
+	cout << word[0] << '\n';
+	for(int i = 1; i < N; i++) {
+		if(word[i-1] != word[i]) {
 			cout << word[i] << '\n';
 		}
 	}
