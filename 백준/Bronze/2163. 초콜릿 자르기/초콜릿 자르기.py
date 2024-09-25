f815a31277547dd7820b778e848d5b82dc92df12
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-09-25 15:09:34 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 112 ms, Memory: 31180 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.py
@@ -0,0 +1,11 @@
+def cutChoco(r, c):
+  if(r == 1 and c == 1):
+    return 0
+  if(r < c):
+    tmp = r
+    r = c
+    c = tmp
+  return 1 + cutChoco(r-1, c) + cutChoco(1, c)
+
+N, M = map(int, input().split())
+print(cutChoco(N, M))
