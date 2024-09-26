f44db5cfa60e82a0d855fa48feeca4941a6e0c94
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-09-26 18:01:49 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int R, C, min, cnt, r, c, i, j;
+	vector<vector<char>> table;
+	
+	cin >> R >> C;
+	table.assign(R, vector<char>(C));
+	getchar();
+	for(r = 0; r < R; r++) {
+		for(c = 0; c < C; c++) {
+			table[r][c] = getchar()=='B' ? 0 : 1;
+		}
+		getchar();
+	}
+	
+	min = R * C;
+	for(r = 0; r < R-7; r++) {
+		for(c = 0; c < C-7; c++) {
+			cnt = 0;
+			for(i = 0; i < 8; i++) {
+				for(j = 0; j < 8; j++) {
+					if(table[r+i][c+j] == (i + j)%2) {
+						cnt++;
+					}
+				}
+			}
+			cnt = cnt < 32 ? cnt : 64-cnt;
+			min = min < cnt ? min : cnt;
+			if(min == 0) {
+				cout << 0;
+				return 0;
+			}
+		}
+	}
+	cout << min;
+	
+	return 0;
+}
