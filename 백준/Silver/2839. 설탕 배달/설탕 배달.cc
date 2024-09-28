13aaa2bc60ff0970a985537a8a42131f5c9f7563
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-09-28 09:33:50 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -2,34 +2,18 @@
 using namespace std;
 
 int main() {
-	int N, five, three = 0;
+	int N, cnt = 0;
 	
 	cin >> N;
-	if(N == 1 || N == 2 || N == 4 || N == 7) {
-		cout << -1;
-		return 0;
+	while(N % 5 > 0) {
+		cnt++;
+		N -= 3;
 	}
-	five = N / 5;
-	switch(N % 5) {
-		case 0:
-			break;
-		case 1:
-			five -= 1;
-			three += 2;
-			break;
-		case 2:
-			five -= 2;
-			three += 4;
-			break;
-		case 3:
-			three += 1;
-			break;
-		case 4:
-			five -= 1;
-			three += 3;
-			break;
+	if(N < 0) {
+		cnt = -1;
 	}
-	cout << five + three;
+	cnt += N / 5;
+	cout << cnt;
 	
 	return 0;
 }
