07f47a4c735122f53fa46d47d2a32975ab6330a1
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-09-29 12:09:04 +0900

    [Silver III] Title: 소수 구하기, Time: 376 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int M, N, i, j;
+	bool chk;
+	vector<int> prime;
+	
+	cin >> M >> N;
+	if(M == 1) M++;
+	if(N == 1) return 0;
+	
+	prime.push_back(2);
+	for(i = 3; i < M; i++) {
+		chk = true;
+		for(j = 0; prime[j]*prime[j] <= i && j < prime.size(); j++) {
+			if(!(i % prime[j])) chk = false;
+		}
+		if(chk) prime.push_back(i);
+	}
+	for(i = M; i <= N; i++) {
+		chk = true;
+		for(j = 0; prime[j]*prime[j] <= i && j < prime.size(); j++) {
+			if(!(i % prime[j])) chk = false;
+		}
+		if(chk) {
+			prime.push_back(i);
+			cout << i << '\n';	
+		}	
+	}
+	
+	return 0;
+}
