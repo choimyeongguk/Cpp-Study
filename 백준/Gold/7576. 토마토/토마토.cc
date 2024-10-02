7d76cf791cc006805e990bef5e00a90f00c5e31c
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-02 16:31:22 +0900

    [Gold V] Title: 토마토, Time: 72 ms, Memory: 10580 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct {
+	int r;
+	int c;
+	int day;
+} BOX;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int C, R, numRaw = 0, r, c, day = 0, i, j;
+	char d[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	vector<vector<short>> tomato;
+	queue<BOX> bfs;
+	
+	cin >> C >> R;
+	tomato.assign(R, vector<short>(C));
+	for(i = 0; i < R; i++) {
+		for(j = 0; j < C; j++) {
+			cin >> tomato[i][j];
+			if(tomato[i][j] == 1) {
+				bfs.push({i, j, 0});
+			}
+			if(tomato[i][j] == 0) {
+				numRaw++;
+			}
+		}
+	}
+
+	while(!bfs.empty()) {
+		r = bfs.front().r;
+		c = bfs.front().c;
+		day = bfs.front().day;
+		//cout << ">> " << r << ' ' << c << ' ' << day << '\n';
+		bfs.pop();
+		
+		for(i = 0; i < 4; i++) {
+			r += d[i][0];
+			c += d[i][1];
+			if(0 <= r && r < R && 0 <= c && c < C && !tomato[r][c]) {
+				tomato[r][c]++;
+				numRaw--;
+				bfs.push({r, c, day + 1});
+			}
+			r -= d[i][0];
+			c -= d[i][1];
+		}
+	}
+	
+	if(numRaw) {
+		cout << -1;
+	} else {
+		cout << day;
+	}
+	
+	return 0;
+}
