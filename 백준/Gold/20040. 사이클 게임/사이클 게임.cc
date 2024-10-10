80f079919cba73eacc136dac5baf47448ffc11c7
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-11 01:37:40 +0900

    [Gold IV] Title: 사이클 게임, Time: 88 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> tree;
+
+int root(int x) {
+	while(x != tree[x]) {
+		x = tree[x];
+	}
+	return x;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n, m, a, b, result = 0, i;
+	
+	cin >> n >> m;
+	tree.assign(n, -1);
+	for(i = 1; i <= m; i++) {
+		cin >> a >> b;
+		if(tree[a] == -1 && tree[b] == -1) {
+			tree[a] = a;  // a를 root 노드로
+			tree[b] = a;  // b를 child 노드로 
+		} else if(tree[a] == -1) {
+			tree[a] = b;
+		} else if(tree[b] == -1) {
+			tree[b] = a;
+		} else {  // 둘 다 부모가 있음 
+			if(root(a) == root(b)) {
+				result = i;
+				break;
+			} else {
+				tree[root(b)] = a;  // b의 root 노드를 a의 child 노드로 
+			}
+		}
+	}
+	
+	cout << result; 
+	
+	return 0;
+}
