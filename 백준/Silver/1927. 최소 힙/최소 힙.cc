fda35f3600a07ac758f50d77fd512fc334d18c48
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-11 01:51:31 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N, cmd, i;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	cin >> N;
+	for(i = 0; i < N; i++) {
+		cin >> cmd;
+		if(cmd) {
+			pq.push(cmd);
+		} else {
+			if(pq.empty()) {
+				cout << 0 << '\n';
+			} else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+	}
+	
+	return 0;
+}
