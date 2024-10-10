e7163d2cea334e38c50df7c9f62097249b8b30ff
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-10 23:41:35 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N, num, order, tmp;
+	queue<pair<int,int>> bfs;  // 숫자, 연산 횟수 
+	vector<bool> visited;
+	
+	cin >> N;
+	bfs.push({ N, 0 });
+	visited.assign(N + 1, false);
+	visited[N] = true;
+	while(!bfs.empty()) {
+		num = bfs.front().first;
+		order = bfs.front().second + 1;
+		if(num == 1) {
+			cout << order - 1;
+			break;
+		}
+		bfs.pop();
+		
+		tmp = num / 3;
+		if(!(num % 3) && !visited[tmp]) {
+			bfs.push({ tmp, order });
+			visited[tmp] = true;
+		}
+		
+		tmp = num / 2;
+		if(!(num % 2) && !visited[tmp]) {
+			bfs.push({ tmp, order });
+			visited[tmp] = true;
+		}
+		
+		tmp = num - 1;
+		if(!visited[tmp]) {
+			bfs.push({ tmp, order });
+			visited[tmp] = true;
+		}
+	}
+	
+	return 0;
+}
