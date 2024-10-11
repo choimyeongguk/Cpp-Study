7f937cc7fb5a4445465cbb99d87bf5a3b6271a9d
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-11 22:33:45 +0900

    [Gold IV] Title: DSLR, Time: 3120 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int D(int x) {
+	return x = (x << 1) % 10000;
+}
+
+int S(int x) {
+	return x = (x + 9999) % 10000;
+}
+
+int L(int x) {
+	return x = (x % 1000) * 10 + x / 1000;
+}
+
+int R(int x) {
+	return x = (x % 10) * 1000 + x / 10;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int T, a, b, num, tmp, i, j;
+	string cmd;
+	vector<bool> visited;
+	queue<pair<int,string>> bfs;
+	int (*func[4])(int) = {D, S, L, R};
+	char funcName[4] = {'D', 'S', 'L', 'R'};
+	
+	cin >> T;
+	for(i = 0; i < T; i++) {
+		cin >> a >> b;
+		
+		visited.assign(10000, false);
+		bfs.push({a, ""});
+		while(bfs.front().first != b) {
+			num = bfs.front().first;
+			cmd = bfs.front().second;
+			bfs.pop();
+			
+			for(j = 0; j < 4; j++) {
+				tmp = func[j](num);
+				if(!visited[tmp]) {
+					visited[tmp] = true;
+					bfs.push({tmp, cmd + funcName[j]});
+				}
+			}
+		}
+		cout << bfs.front().second << "\n";
+		while(!bfs.empty()) bfs.pop();
+	}
+	
+	return 0;
+}
