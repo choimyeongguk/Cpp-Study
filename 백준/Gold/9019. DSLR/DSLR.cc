094aa9c0b36df8c7e8b9bdd3631f9059ad6235f3
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-11 23:12:01 +0900

    [Gold IV] Title: DSLR, Time: 1640 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -24,31 +24,41 @@ int main() {
 	
 	int T, a, b, tmp, i, j;
 	vector<bool> visited;
-	queue<pair<int,string>> bfs;
+	queue<pair<int,int>> bfs;
 	int (*func[4])(int) = {D, S, L, R};
 	char funcName[4] = {'D', 'S', 'L', 'R'};
+	stack<char> result;
 	
 	cin >> T;
 	for(i = 0; i < T; i++) {
 		cin >> a >> b;
 		
 		visited.assign(10000, false);
-		bfs.push({a, ""});
+		bfs.push({a, 1});
 		while(bfs.front().first != b) {
 			int& num = bfs.front().first;
-			string& cmd = bfs.front().second;
+			int& cmd = bfs.front().second;
 			
 			for(j = 0; j < 4; j++) {
 				tmp = func[j](num);
 				if(!visited[tmp]) {
 					visited[tmp] = true;
-					bfs.push({tmp, cmd + funcName[j]});
+					bfs.push({tmp, cmd*4 + j});
 				}
 			}
 			bfs.pop();
 		}
-		cout << bfs.front().second << "\n";
+		tmp = bfs.front().second;
 		while(!bfs.empty()) bfs.pop();
+		while(tmp != 1) {
+			result.push(funcName[tmp%4]);
+			tmp /= 4;
+		}	
+		while(!result.empty()) {
+			cout << result.top();
+			result.pop();
+		}
+		cout << "\n";
 	}
 	
 	return 0;
