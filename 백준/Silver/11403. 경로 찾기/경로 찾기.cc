a00e477cb4d079a9810f08631d02686fec2fb5b4
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-15 22:07:39 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N, tmp, i, j;
+	vector<vector<bool>> graph;
+	
+	cin >> N;
+	graph.assign(N, vector<bool>(N, false));
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < N; j++) {
+			cin >> tmp;
+			graph[i][j] = tmp;
+		}
+	}
+	
+	for(int m = 0; m < N; m++) {
+		for(int s = 0; s < N; s++) {
+			for(int e = 0; e < N; e++) {
+				if(graph[s][m] && graph[m][e]) {
+					graph[s][e] = true;
+				}
+			}
+		}
+	}
+	
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < N; j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
