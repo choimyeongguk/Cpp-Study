ae74bba93f58e06d5a1c00eaf8b58e869de41647
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-22 18:21:31 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -3,18 +3,17 @@ using namespace std;
 
 int N, r, c;
 
-int dfs(int L) {
+int Z(int L) {
 	if(L == 1) return 0;
 	int l = L >> 1, i = 0;
 	if(r >= l && c >= l) i = 3, r -= l, c -= l;
 	else if(r >= l) 	 i = 2, r -= l;
 	else if(c >= l)		 i = 1, c -= l;
-	return l * l * i + dfs(l);
+	return l * l * i + Z(l);
 }
 
 int main() {
 	cin >> N >> r >> c;
-	cout << dfs(1 << N);
-	
+	cout << Z(1 << N);
 	return 0;
 }
