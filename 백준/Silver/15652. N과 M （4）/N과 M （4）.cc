530a110d9e57a26a1bcaf75f724906e49c365561
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-23 00:11:42 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -4,18 +4,17 @@ using namespace std;
 int N, M;
 vector<int> arr;
 
-void solution(int x) {
-	if(arr.size() == M) {
-		for(auto& e : arr) {
-			cout << e << " ";
+void solution(int depth) {
+	if(depth == M + 1) {
+		for(int i = 1; i <= M; i++) {
+			cout << arr[i] << " ";
 		}
 		cout << "\n";
 		return;
 	}
-	for(int i = x; i <= N; i++) {
-		arr.push_back(i);
-		solution(i);
-		arr.pop_back();
+	for(int i = arr[depth - 1]; i <= N; i++) {
+		arr[depth] = i;
+		solution(depth + 1);
 	}
 }
 
@@ -25,6 +24,8 @@ int main() {
 	cout.tie(0);
 	
 	cin >> N >> M;
+	arr.resize(M + 1);
+	arr[0] = 1;
 	solution(1);
 	
 	return 0;
