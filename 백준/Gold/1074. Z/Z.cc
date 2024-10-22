9e36b74bdf20820987ce1a76cdc9121fe9612755
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-22 18:03:23 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -3,15 +3,15 @@ using namespace std;
 
 int r, c;
 
-int dfs(int R, int C, int N) {
+int dfs(int R, int C, int size) {
 	if(R == r && C == c) {
 		return 0;
 	}
-	int ans = 0, n = pow(2, N - 1);
-	if     (r < R + n && c < C + n)	 ans += dfs(R, C, N - 1);
-	else if(r < R + n && c >= C + n) ans += n * n + dfs(R, C + n, N - 1);
-	else if(r >= R + n && c < C + n) ans += n * n * 2 + dfs(R + n, C, N - 1);
-	else							 ans += n * n * 3 + dfs(R + n, C + n, N - 1);
+	int ans = 0, n = size / 2;
+	if     (r < R + n && c < C + n)	 ans += dfs(R, C, n);
+	else if(r < R + n && c >= C + n) ans += n * n + dfs(R, C + n, n);
+	else if(r >= R + n && c < C + n) ans += n * n * 2 + dfs(R + n, C, n);
+	else							 ans += n * n * 3 + dfs(R + n, C + n, n);
 	
 	return ans;
 }
@@ -24,7 +24,7 @@ int main() {
 	int N;
 	
 	cin >> N >> r >> c;
-	cout << dfs(0, 0, N);
+	cout << dfs(0, 0, pow(2, N));
 	
 	return 0;
 }
