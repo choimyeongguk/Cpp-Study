1225817857d267e3eb20764f5ffba851dec98b32
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-26 11:38:54 +0900

    [Silver II] Title: 과일 탕후루, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
@@ -6,28 +6,23 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 	
-	int N, ans, p1, p2, f1, f2, i;
+	int N, ans, l, r, fl, fr, i;
 	vector<char> fruit;
 	
 	cin >> N;
-	
 	fruit.resize(N);
-	for(auto& e : fruit) {
-		cin >> e;
-	}
+	for(auto& e : fruit) cin >> e;
 	
-	p1 = p2 = 0;
-	f1 = f2 = fruit[0];
-	ans = 1;
-	for(i = 1; i < N; i++) {
-		p2 = i;
-		if(fruit[i] != f1 && fruit[i] != f2) {
-			f2 = fruit[i];
-			f1 = fruit[i - 1];
-			for(p1 = i - 1; fruit[p1] == f1 || fruit[p1] == f2; p1--);
-			p1++;
+	ans = l = -1;
+	fl = fr = fruit[0];
+	for(i = 0; i < N; i++) {
+		r = i;
+		if(fruit[i] != fl && fruit[i] != fr) {
+			fr = fruit[i];
+			fl = fruit[i - 1];
+			for(l = i; fruit[l] == fl || fruit[l] == fr; l--);
 		}
-		if(ans < p2 - p1 + 1) ans = p2 - p1 + 1;
+		if(ans < r - l) ans = r - l;
 	}
 	cout << ans;
 	
