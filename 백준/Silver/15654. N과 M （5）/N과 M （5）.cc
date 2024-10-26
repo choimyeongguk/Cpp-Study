f6199f21ab962b689fb70ae92a6b028aec363df6
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-27 00:45:55 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> seq;
+vector<int> idx;
+vector<bool> used;
+
+void comb(int start, int depth) {
+	if(depth == M) {
+		for(auto& i : idx) cout << seq[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for(int i = 0; i < N; i++) {
+		if(used[i]) continue;
+		idx.push_back(i);
+		used[i] = true;
+		comb(i, depth + 1);
+		idx.pop_back();
+		used[i] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> N >> M;
+	seq.resize(N);
+	used.assign(N, false);
+	for(auto& e : seq) cin >> e;
+	sort(seq.begin(), seq.end());
+	comb(-1, 0);
+	
+	return 0;
+}
