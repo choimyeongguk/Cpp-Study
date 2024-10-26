feb0573f8e4ef98c43b389a530b26ff7c06e6b5d
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-27 00:33:52 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long int lld;
 
 lld dnq(lld A, lld B, lld C) {
 	if(B == 1) return A % C;
-	if(B % 2) {  // 홀수 
-		return (dnq(A, B/2, C) * dnq(A, B - B/2, C)) % C;
-	}
+	
 	lld tmp = dnq(A, B/2, C);
+	if(B % 2) {  // 홀수 
+		return ((tmp * tmp ) % C * A) % C;
+	}
 	return (tmp * tmp) % C;
 }
 
