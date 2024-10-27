ad8e5b7b324917232528470b4f635a64803f2050
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-27 10:43:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -7,20 +7,20 @@ int main() {
 	cout.tie(0);
 	
 	int N, max, i, j;
-	cin >> N;
-	vector<vector<int>> tri(N, vector<int>(N, 0));
+	int tri[500][500] = { 0 };
 	
+	cin >> N;
 	cin >> tri[0][0];
 	for(i = 1; i < N; i++) {
 		for(j = 0; j <= i; j++) {
 			cin >> tri[i][j];
 			max = j > 0 ? tri[i - 1][j - 1] : 0;
-			max = max > tri[i - 1][j] ? max : tri[i - 1][j];
+			if(max < tri[i - 1][j]) max = tri[i - 1][j];
 			tri[i][j] += max;
 		}
 	}
 	for(max = 0, i = 0; i < N; i++) {
-		max = max > tri[N - 1][i] ? max : tri[N - 1][i];
+		if(max < tri[N - 1][i]) max = tri[N - 1][i];
 	}
 	cout << max << "\n";
 	
