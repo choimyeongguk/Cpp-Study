58439776ea018e3f644bc6fb919c28f9374f2c40
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-27 18:53:54 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 116 ms, Memory: 6268 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -14,16 +14,12 @@ int main() {
 	for(i = 1; i <= N; i++) {
 		for(j = 1; j <= N; j++) {
 			cin >> table[i][j];
-			table[i][j] += table[i][j - 1];
+			table[i][j] += table[i][j - 1] + table[i - 1][j] - table[i - 1][j - 1];
 		}
 	}
 	for(i = 0; i < M; i++) {
-		cin >> y1 >> x1 >> y2 >> x2;
-		ans = 0;
-		for(j = y1; j <= y2; j++) {
-			ans += table[j][x2] - table[j][x1 - 1];
-		}
-		cout << ans << "\n";
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << table[x2][y2] - table[x2][y1 - 1] - table[x1 - 1][y2] + table[x1 - 1][y1 - 1] << "\n";
 	}
 	
 	return 0;
