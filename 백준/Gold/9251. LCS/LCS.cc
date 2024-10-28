27fce4390cdc1c3cae02b6d8b20d188667c7636f
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-28 13:09:29 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int ans = 0;
+	string s1, s2;
+	vector<vector<int>> dp;
+	
+	cin >> s1 >> s2;
+	dp.assign(s1.length() + 1, vector<int>(s2.length() + 1, 0));
+	for(int i = 1; i <= s1.length(); i++) {
+		for(int j = 1; j <= s2.length(); j++) {
+			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			if(s1[i - 1] == s2[j - 1]) {
+				dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j - 1]);
+			}
+			ans = max(ans, dp[i][j]);
+		}
+	}
+	cout << ans;
+	
+	return 0;
+}
