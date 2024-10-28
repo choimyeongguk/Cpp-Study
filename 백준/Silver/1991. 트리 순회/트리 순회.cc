2169c2a25c7e63e329b4c3053e5703b716b257f2
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-28 12:33:54 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+	char left;
+	char right;
+};
+
+vector<node> tree(91);
+
+void preorder(char node) {
+	if(node == '.') return;
+	cout << node;
+	preorder(tree[node].left);
+	preorder(tree[node].right);
+}
+
+void inorder(char node) {
+	if(node == '.') return;
+	inorder(tree[node].left);
+	cout << node;
+	inorder(tree[node].right);
+}
+
+void postorder(char node) {
+	if(node == '.') return;
+	postorder(tree[node].left);
+	postorder(tree[node].right);
+	cout << node;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N, i;
+	char parent, left, right;
+	
+	cin >> N;
+	for(i = 0; i < N; i++) {
+		cin >> parent >> left >> right;
+		tree[parent] = { left, right };
+	}
+	
+	preorder('A'); cout << "\n";
+	inorder('A'); cout << "\n";
+	postorder('A');
+	
+	return 0;
+}
