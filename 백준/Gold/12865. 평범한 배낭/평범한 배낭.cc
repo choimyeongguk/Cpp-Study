472db0f3b52f3351efb1ecc3b656e8a5234bd02d
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-29 10:09:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,36 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Stuff {
-	int w, v;
-};
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	
-	int N, K, i, j;
-	vector<Stuff> stuff;
-	vector<vector<int>> dp;
+	int N, K, w, v, i, j;
+	int dp[100001] = { 0 };
 	
 	cin >> N >> K;
 	
-	stuff.assign(N + 1, {0, 0});
-	for(i = 1; i <= N; i++) {
-		cin >> stuff[i].w >> stuff[i].v;
-	}
-	
-	dp.assign(K + 1, vector<int>(N + 1, 0));
-	for(i = 1; i <= K; i++) {
-		for(j = 1; j <= N; j++) {
-			if(i >= stuff[j].w)
-				dp[i][j] = max(dp[i][j - 1], stuff[j].v + dp[i - stuff[j].w][j - 1]);
-			else
-				dp[i][j] = dp[i][j - 1];	
+	for(i = 0; i < N; i++) {
+		cin >> w >> v;
+		for(j = K; j >= w; j--) {
+			dp[j] = max(dp[j], dp[j - w] + v);
 		}
 	}
-	cout << dp[K][N];
+	cout << dp[K];
 	
 	return 0;
 }
