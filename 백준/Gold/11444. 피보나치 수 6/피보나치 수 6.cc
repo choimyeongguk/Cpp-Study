5bcaa27595bee24ba86cbd61d6bf761413d1b896
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-30 22:35:01 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -1,40 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<long long, long long> dp;
+typedef long long ll;
+map<ll,ll> dp;
 
-long long f(long long n) {
+ll fib(ll n) {
 	if(n == 0) return 0;
 	if(n == 1) return 1;
 	if(dp.find(n) != dp.end()) return dp[n];
 	
-	long long f1, f2;
+	ll f1 = fib(n / 2);
+	ll f2 = fib(n / 2 - 1);
 	
-	if(dp.find(n / 2) != dp.end()) {
-		f1 = dp[n / 2];
-	} else {
-		f1 = f(n / 2);
-		dp.insert({n / 2, f1});
-	}
-	if(dp.find(n / 2 - 1) != dp.end()) {
-		f2 = dp[n / 2 - 1];
-	} else {
-		f2 = f(n / 2 - 1);
-		dp.insert({n / 2 - 1, f2});
-	}
-	
-	if(n % 2 == 0) {
-		dp[n] = (f1 * f1 + 2 * f1 * f2) % 1000000007;
-		return dp[n];
-	} else {
-		dp[n] = (2 * f1 * (f1 + f2) + f2 * f2) % 1000000007;
-		return dp[n];
-	}
+	if(n % 2 == 0) 
+		return dp[n] = (f1 * f1 + 2 * f1 * f2) % 1000000007;
+	else
+		return dp[n] = (2 * f1 * (f1 + f2) + f2 * f2) % 1000000007;
 }
 
 int main() {
-	long long n;
+	ll n;
 	cin >> n;
-	cout << f(n);
+	cout << fib(n);
 	return 0;
 }
