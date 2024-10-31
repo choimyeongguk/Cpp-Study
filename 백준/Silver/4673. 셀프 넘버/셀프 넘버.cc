a47b3f880f8132734eb084c19e6574fe7790d3f0
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-31 17:49:42 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -13,15 +13,10 @@ int d(int n) {
 int main() {
 	ios_base::sync_with_stdio(0);
 	cout.tie(0);
-	
 	vector<bool> isSelf(10036, true);
-	
 	for(int i = 1; i <= 10000; i++) {
 		isSelf[d(i)] = false;
-	}
-	for(int i = 1; i <= 10000; i++) {
 		if(isSelf[i]) cout << i << "\n";
 	}
-	
 	return 0;
 }
