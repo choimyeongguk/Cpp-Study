a1be5c0085b6fa6ec00c538085cc6f4d744b606f
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-10-31 17:32:37 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int cnt = 0;
+	string word;
+	cin >> word;
+	for(int i = 0; i < word.length(); i++) {
+		cnt++;
+		switch(word[i]) {
+			case 'c' : case 's' : case 'z' :
+				if(word[i + 1] == '=' || word[i + 1] == '-') i++;
+				break;
+			case 'd' :
+				if(i + 2 < word.length() && word[i + 1] == 'z' && word[i + 2] == '=') i += 2;
+				if(word[i + 1] == '-') i++;
+				break;
+			case 'l' : case 'n':
+				if(word[i + 1] == 'j') i++;
+				break;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
