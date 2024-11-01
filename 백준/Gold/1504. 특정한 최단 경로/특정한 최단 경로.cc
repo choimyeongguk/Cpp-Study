7f82a8d9279fccd8fb596b77e03474e6dc318390
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-01 11:14:17 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 32 ms, Memory: 4676 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -4,16 +4,37 @@ using namespace std;
 
 typedef pair<int,int> pii;
 
+int N;
+vector<vector<int>> graph;
+vector<int> dist;
+
+void dijkstra(int start) {
+	int c, n, i;
+	priority_queue<pii, vector<pii>, greater<pii>> heap;
+	dist.assign(N + 1, INF);
+	dist[start] = 0;
+	heap.push({0, start});
+	while(!heap.empty()) {
+		c = heap.top().first;
+		n = heap.top().second;
+		heap.pop();
+		if(dist[n] < c) continue;
+		for(i = 1; i <= N; i++) {
+			if(dist[i] > c + graph[n][i]) {
+				dist[i] = c + graph[n][i];
+				heap.push({dist[i], i});
+			}
+		}
+	}
+}
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	
-	int N, E, a, b, c, v1, v2, n, i;
+	int E, a, b, c, v1, v2, n, t1, t2, i;
 	int StoV1, StoV2, VtoV, V1toE, V2toE;
-	vector<vector<int>> graph;
-	vector<int> dist;
-	priority_queue<pii, vector<pii>, greater<pii>> heap;
 	
 	cin >> N >> E;
 	graph.assign(N + 1, vector<int>(N + 1, INF));
@@ -25,66 +46,19 @@ int main() {
 	}
 	cin >> v1 >> v2;
 	
-	dist.assign(N + 1, INF);
-	dist[1] = 0;
-	heap.push({0, 1});
-	while(!heap.empty()) {
-		c = heap.top().first;
-		n = heap.top().second;
-		heap.pop();
-		if(dist[n] < c) continue;
-		for(i = 1; i <= N; i++) {
-			if(dist[i] > c + graph[n][i]) {
-				dist[i] = c + graph[n][i];
-				heap.push({dist[i], i});
-			}
-		}
-	}
+	dijkstra(1);
 	StoV1 = dist[v1];
 	StoV2 = dist[v2];
 	
-	dist.assign(N + 1, INF);
-	dist[v1] = 0;
-	heap.push({0, v1});
-	while(!heap.empty()) {
-		c = heap.top().first;
-		n = heap.top().second;
-		heap.pop();
-		if(dist[n] < c) continue;
-		for(i = 1; i <= N; i++) {
-			if(dist[i] > c + graph[n][i]) {
-				dist[i] = c + graph[n][i];
-				heap.push({dist[i], i});
-			}
-		}
-	}
+	dijkstra(v1);
 	VtoV = dist[v2];
 	V1toE = dist[N];
 	
-	dist.assign(N + 1, INF);
-	dist[v2] = 0;
-	heap.push({0, v2});
-	while(!heap.empty()) {
-		c = heap.top().first;
-		n = heap.top().second;
-		heap.pop();
-		if(dist[n] < c) continue;
-		for(i = 1; i <= N; i++) {
-			if(dist[i] > c + graph[n][i]) {
-				dist[i] = c + graph[n][i];
-				heap.push({dist[i], i});
-			}
-		}
-	}
+	dijkstra(v2);
 	V2toE = dist[N];
 	
-//	cout << "StoV1 : " << StoV1 << "\n";
-//	cout << "StoV2 : " << StoV2 << "\n";
-//	cout << "VtoV : " << VtoV << "\n";
-//	cout << "V1toE : " << V1toE << "\n";
-//	cout << "V2toE : " << V2toE << "\n";
-	int t1 = StoV1 + VtoV + V2toE;
-	int t2 = StoV2 + VtoV + V1toE;
+	t1 = StoV1 + VtoV + V2toE;
+	t2 = StoV2 + VtoV + V1toE;
 	if(t1 >= INF && t2 >= INF) cout << "-1";
 	else cout << (t1 < t2 ? t1 : t2);
 	
