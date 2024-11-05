6b1813972cbc66628cd0ecd126532b3f5a368bfc
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-05 11:56:28 +0900

    [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.cc
+++ 백준/Gold/1043. 거짓말/거짓말.cc
@@ -1,51 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int tree[51];  // tree[i]=j : i노드의 부모는 j 
+int level[51];  // tree의 높이 
+
+int root(int x) {
+	if(x == tree[x]) return x;
+	return tree[x] = root(tree[x]);
+}
+
+void combine(int x, int y) {
+	if(x == y) return;
+	if(!x) {
+		tree[y] = x;
+		return;
+	}
+	if(!y) {
+		tree[x] = y;
+		return;
+	}
+	if(level[x] > level[y]) tree[y] = x;
+	else tree[x] = y;
+	if(level[x] == level[y]) level[y]++;
+}
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	
-	int N, M, num, tmp, ans, i, j, k;
-	int group[51][51];  // group[i][j]=k : i번째 파티의 j번째 사람은 k번 
+	int N, M, num, tmp, ans, i, j;
+	int group[51];
 	
-	cin >> N >> M >> num;
-
-	vector<int> knowTrue(num);
-	vector<vector<int>> graph(N + 1);
+	cin >> N >> M;
+	
+	for(i = 0; i <= N; i++) {
+		tree[i] = i;
+		level[i] = 1;
+	}
+	level[0] = 2;
+	cin >> num;
 	for(i = 0; i < num; i++) {
-		cin >> knowTrue[i];
+		cin >> tmp;
+		tree[tmp] = 0;  // 진실을 아는 집합의 루트는 0 
+	}
+	
+	for(ans = 0, i = 0; i < M; i++) {
+		cin >> num >> group[i];
+		for(j = 1; j < num; j++) {
+			cin >> tmp;
+			combine(root(group[i]), root(tmp));
+		}
 	}
 	for(i = 0; i < M; i++) {
-		cin >> num;
-		for(j = 0; j < num; j++) {
-			cin >> group[i][j];
-		}
-		for(j = 0; j < num; j++) {
-			for(k = 0; k < num; k++) {
-				graph[group[i][j]].push_back(group[i][k]);
-			}
-		}
+		if(root(tree[group[i]])) ans++;
 	}
 	
-	vector<int> visited(N + 1, false);
-	
-	queue<int> bfs;
-	for(auto& e : knowTrue) {
-		bfs.push(e);
-		while(!bfs.empty()) {
-			int& n = bfs.front();
-			for(i = 0; i < graph[n].size(); i++) {
-				if(visited[graph[n][i]]) continue;
-				visited[graph[n][i]] = true;
-				bfs.push(graph[n][i]);
-			}
-			bfs.pop();
-		}
-	}
-	
-	for(ans = 0, i = 0; i < M; i++)
-		if(!visited[group[i][0]]) ans++;
 	cout << ans;
 	
 	return 0;
