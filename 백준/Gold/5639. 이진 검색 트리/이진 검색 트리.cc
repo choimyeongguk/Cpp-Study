53d9e7d9c8e36c1577018773e2976b9860d2d1e2
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-06 16:28:51 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 84 ms, Memory: 2448 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -1,34 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class binarySearch_tree {
-	public:
-		int key;
-		binarySearch_tree *left;
-		binarySearch_tree *right;
-	
-	binarySearch_tree(int val) {
-		key = val;
-		left = NULL;
-		right = NULL;
+struct binarySearch_tree {
+	int key;
+	binarySearch_tree* left;
+	binarySearch_tree* right;
+};
+
+binarySearch_tree* createNode(int val) {
+	binarySearch_tree* newNode = new binarySearch_tree;
+	newNode->key = val;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+void insert(binarySearch_tree* root, int val) {
+	binarySearch_tree* node = root;
+	if(node == NULL) {
+		node = createNode(val);
+		return;
 	}
-	
-	void insert(int val, binarySearch_tree *node) {
+	while(true) {
 		if(val < node->key) {
 			if(node->left == NULL) {
-				node->left = new binarySearch_tree(val);
-			} else {
-				insert(val, node->left);
-			}
+				node->left = createNode(val);
+				break;
+			} else node = node->left;
 		} else {
 			if(node->right == NULL) {
-				node->right = new binarySearch_tree(val);
-			} else {
-				insert(val, node->right);
-			}
+				node->right = createNode(val);
+				break;
+			} else node = node->right;
 		}
 	}
-};
+}
 
 void postorder(binarySearch_tree *node) {
 	if(node->left != NULL) postorder(node->left);
@@ -43,15 +48,15 @@ int main() {
 	
 	int num;
 	cin >> num;
-	binarySearch_tree tree = binarySearch_tree(num);
+	binarySearch_tree* tree = createNode(num);
 	
 	while(true) {
 		cin >> num;
 		if(cin.eof()) break;
-		tree.insert(num, &tree);
+		insert(tree, num);
 	}
 	
-	postorder(&tree);
+	postorder(tree);
 	
 	return 0;
 }
