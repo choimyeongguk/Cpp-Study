7b957fc4d2d1851cfcb106d3e0f5e4db0011b669
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-06 12:07:39 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 404 ms, Memory: 20620 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<char>> tri;
+
+void solve(int r, int c, int n) {
+	if(n == 3) {
+		tri[r][c] = '*';
+		tri[r + 1][c - 1] = tri[r + 1][c + 1] = '*';
+		tri[r + 2][c - 2] = tri[r + 2][c - 1] = tri[r + 2][c] = tri[r + 2][c + 1] = tri[r + 2][c + 2] = '*';
+		return;
+	}
+	n /= 2;
+	solve(r, c, n);
+	solve(r + n, c - n, n);
+	solve(r + n, c + n, n);
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N;
+	cin >> N;
+	tri.assign(N, vector<char>(2 * N - 1, ' '));
+	solve(0, N - 1, N);
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < 2 * N - 1; j++) {
+			cout << tri[i][j];
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
