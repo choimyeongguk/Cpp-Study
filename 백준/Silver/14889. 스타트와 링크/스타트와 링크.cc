2e8b8a3b38b494f04b7c3bcd9fc2c61f58d325f9
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-10 11:19:38 +0900

    [Silver I] Title: 스타트와 링크, Time: 116 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -6,26 +6,22 @@ int M[21][21];
 vector<char> player;
 
 void compare() {
-	int i, j;
 	int team[2] = { 0 };
-	for(i = 0; i < N; i++) {
-		for(j = i + 1; j < N; j++) {
-			if(player[i] == player[j]) {
+	for(int i = 0; i < N; i++)
+		for(int j = i + 1; j < N; j++)
+			if(player[i] == player[j])
 				team[player[i]] += M[i][j];
-			}
-		}
-	}
 	ans = min(ans, abs(team[0] - team[1]));
 }
 
 void comb(int start, int depth) {
-	if(depth == N / 2) {
+	if(depth++ == N >> 1) {
 		compare();
 		return;
 	}
 	for(int i = start; i < N; i++) {
 		player[i] = 1;
-		comb(i + 1, depth + 1);
+		comb(i + 1, depth);
 		player[i] = 0;
 	}
 }
@@ -35,18 +31,14 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 	
-	int i, j;
-	
 	cin >> N;
-	
 	player.resize(N, 0);
-	for(i = 0; i < N; i++)
-		for(j = 0; j < N; j++)
+	for(int i = 0; i < N; i++)
+		for(int j = 0; j < N; j++)
 			cin >> M[i][j];
-	for(i = 0; i < N; i++)
-		for(j = i + 1; j < N; j++)
+	for(int i = 0; i < N; i++)
+		for(int j = i + 1; j < N; j++)
 			M[i][j] += M[j][i];
-	
 	comb(0, 0);
 	cout << ans;
 	
