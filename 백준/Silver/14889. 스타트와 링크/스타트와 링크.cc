b3c5f41e33d0c519c666a2ee75040f2317312ae6
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-10 12:03:13 +0900

    [Silver I] Title: 스타트와 링크, Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -3,35 +3,28 @@ using namespace std;
 
 int N, ans = 0x3f3f3f3f;
 int M[21][21];
-char player[21];
+vector<int> team0, team1;
 
-void compare() {
-	vector<int> team0;
-	vector<int> team1;
-	int team[2] = { 0 };
-	for(int i = 0; i < N; i++) {
-		if(player[i]) team1.push_back(i);
-		else team0.push_back(i);
-	}
-	for(int i = 0; i < team0.size(); i++) {
-		for(int j = 0; j < team0.size(); j++) {
-			team[0] += M[team0[i]][team0[j]];
-			team[1] += M[team1[i]][team1[j]];
-		}
-	}
-	ans = min(ans, abs(team[0] - team[1]));
-}
-
-void comb(int start, int depth) {
-	if(depth++ == N >> 1) {
-		compare();
+void solve(int depth) {
+	if(depth == N) {
+		if(team0.size() != N / 2) return;
+		int team[2] = { 0 };
+		for(int i = 0; i < N / 2; i++)
+			for(int j = 0; j < N / 2; j++) {
+				team[0] += M[team0[i]][team0[j]];
+				team[1] += M[team1[i]][team1[j]];
+			}
+		ans = min(ans, abs(team[0] - team[1]));
 		return;
 	}
-	for(int i = start; i < N; i++) {
-		player[i] = 1;
-		comb(i + 1, depth);
-		player[i] = 0;
-	}
+	
+	team0.push_back(depth);
+	solve(depth + 1);
+	team0.pop_back();
+	
+	team1.push_back(depth);
+	solve(depth + 1);
+	team1.pop_back();
 }
 
 int main() {
@@ -43,7 +36,7 @@ int main() {
 	for(int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
 			cin >> M[i][j];
-	comb(0, 0);
+	solve(0);
 	cout << ans;
 	
 	return 0;
