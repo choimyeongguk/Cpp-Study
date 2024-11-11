99890da4b13cc26485de43ae6a53b0687ef91a0f
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-11 10:37:33 +0900

    [Silver I] Title: 안전 영역, Time: 24 ms, Memory: 2724 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M[101][101];
+vector<vector<bool>> visited;
+
+void dfs(int r, int c, int h) {
+	char d[2][4] = {{-1, 1, 0, 0},{0, 0, -1, 1}};
+	int nr, nc;
+	for(int i = 0; i < 4; i++) {
+		nr = r + d[0][i];
+		nc = c + d[1][i];
+		if(0 <= nr&&nr < N && 0 <= nc&&nc < N && M[nr][nc] > h && !visited[nr][nc]) {
+			visited[nr][nc] = true;
+			dfs(nr, nc, h);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int min = 100, max = 1, cnt, ans, i, j, h;
+	
+	cin >> N;
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < N; j++) {
+			cin >> M[i][j];
+			min = min < M[i][j] ? min : M[i][j];
+			max = max > M[i][j] ? max : M[i][j];
+		}
+	}
+	for(ans = 0, h = min - 1; h < max; h++) {
+		visited.assign(N, vector<bool>(N, false));
+		for(cnt = 0, i = 0; i < N; i++) {
+			for(j = 0; j < N; j++) {
+				if(M[i][j] > h && !visited[i][j]) {
+					cnt++;
+					visited[i][j] = true;
+					dfs(i, j, h);
+				}
+			}
+		}
+		ans = ans > cnt ? ans : cnt;
+	}
+	cout << ans;
+	
+	return 0;
+}
