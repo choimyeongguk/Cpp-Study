79a90d846c5d4e4665d21a0f7fa3343516dd0b0f
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-20 10:27:06 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 92 ms, Memory: 6820 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+vector<vector<char>> image;
+
+void solve(int r, int c, int s) {
+	if(s == 3) {
+		image[r][c] = image[r][c + 1] = image[r][c + 2] = '*';
+		image[r + 1][c] = image[r + 1][c + 2] = '*';
+		image[r + 2][c] = image[r + 2][c + 1] = image[r + 2][c + 2] = '*';
+		return;
+	}
+	s /= 3;
+	solve(r, c, s);
+	solve(r, c + s, s);
+	solve(r, c + s + s, s);
+	solve(r + s, c, s);
+	solve(r + s, c + s + s, s);
+	solve(r + s + s, c, s);
+	solve(r + s + s, c + s, s);
+	solve(r + s + s, c + s + s, s);
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> N;
+	image.assign(N, vector<char>(N, ' '));
+	solve(0, 0, N);
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			cout << image[i][j];
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
