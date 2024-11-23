c0f247e8324d4cb00278a80149d182487eca3167
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-23 13:24:03 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2680 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N, min, i;
+	int l[100001];
+	int p[100001];
+	long long ans = 0;
+	
+	cin >> N;
+	for(i = 1; i < N; i++)
+		cin >> l[i];
+	for(i = 0; i < N; i++)
+		cin >> p[i];
+	
+	min = p[0];
+	for(i = 1; i < N; i++) {
+		ans += l[i] * min;
+		if(min > p[i])
+			min = p[i];
+	}
+	cout << ans;
+	
+	return 0;
+}
