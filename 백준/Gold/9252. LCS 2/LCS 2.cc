b3697490c61777712b43160b6c64bf0ce1bef9cb
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-24 12:25:54 +0900

    [Gold IV] Title: LCS 2, Time: 12 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int i, j;
+	string A, B;
+	vector<vector<pair<int,pair<int,int>>>> dp;
+	stack<char> S;
+	
+	cin >> A >> B;
+	dp.assign(B.size() + 1, vector<pair<int,pair<int,int>>>(A.size() + 1, {0, {0, 0}}));
+	
+	for(i = 1; i <= B.size(); i++) {
+		for(j = 1; j <= A.size(); j++) {
+			if(B[i - 1] == A[j - 1]) {
+				dp[i][j] = {dp[i - 1][j - 1].first + 1, {i - 1, j - 1}};
+			} else if(dp[i - 1][j].first > dp[i][j - 1].first) {
+				dp[i][j] = {dp[i - 1][j].first, {i - 1, j}};
+			} else {
+				dp[i][j] = {dp[i][j - 1].first, {i, j - 1}};
+			}
+		}
+	}
+	
+//	for(i = 1; i <= B.size(); i++) {
+//		for(j = 1; j <= A.size(); j++) {
+//			cout << dp[i][j].first << " ";
+//		}
+//		cout << "\n";
+//	}
+	cout << dp[B.size()][A.size()].first << "\n";
+	int ti, tj;
+	i = B.size(), j = A.size();
+	while(i > 0 && j > 0) {
+		if(B[i - 1] == A[j - 1]) {
+			S.push(A[j - 1]);
+		}
+		ti = dp[i][j].second.first;
+		tj = dp[i][j].second.second;
+		i = ti, j = tj;
+	}
+	while(!S.empty()) {
+		cout << S.top();
+		S.pop();
+	}
+	
+	return 0;
+}
