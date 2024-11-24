b0428a90fa4cfb5cda983e937ba9c89e1a46f707
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-24 12:37:54 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5816 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -8,31 +8,29 @@ int main() {
 	
 	int i, j;
 	string A, B;
-	vector<vector<pair<int,pair<int,int>>>> dp;
+	int dp[1001][1001] = { 0 };
 	stack<char> S;
 	
 	cin >> A >> B;
-	dp.assign(B.size() + 1, vector<pair<int,pair<int,int>>>(A.size() + 1, {0, {0, 0}}));
 	
 	for(i = 1; i <= B.size(); i++) {
 		for(j = 1; j <= A.size(); j++) {
-			if(B[i - 1] == A[j - 1]) {
-				dp[i][j] = {dp[i - 1][j - 1].first + 1, {i - 1, j - 1}};
-			} else if(dp[i - 1][j].first > dp[i][j - 1].first) {
-				dp[i][j] = {dp[i - 1][j].first, {i - 1, j}};
-			} else {
-				dp[i][j] = {dp[i][j - 1].first, {i, j - 1}};
-			}
+			if(B[i - 1] == A[j - 1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 		}
 	}
 	
-	cout << dp[B.size()][A.size()].first << "\n";
-	pair<int,int> idx = { B.size(), A.size() };
-	while(idx.first > 0 && idx.second > 0) {
-		if(B[idx.first - 1] == A[idx.second - 1]) {
-			S.push(A[idx.second - 1]);
+	cout << dp[B.size()][A.size()] << "\n";
+	i = B.size(), j = A.size();
+	while(i > 0 && j > 0) {
+		if(B[i - 1] == A[j - 1]) {
+			S.push(A[j - 1]);
+			i--, j--;
+		} else {
+			dp[i - 1][j] > dp[i][j - 1] ? i-- : j--;
 		}
-		idx = dp[idx.first][idx.second].second;
 	}
 	while(!S.empty()) {
 		cout << S.top();
