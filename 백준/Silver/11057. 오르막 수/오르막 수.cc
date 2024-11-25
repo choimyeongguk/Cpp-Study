033dbf36b6a557181f91343fc143d0ccf02b34f5
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-25 13:21:59 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -4,12 +4,11 @@ using namespace std;
 int main() {
 	int N, i, j, k, ans = 0;
 	int dp[1001][10] = { 0 };
+	
 	cin >> N;
-	for(i = 0; i <= 9; i++) {
-		dp[1][i] = 1;
-	}
-		
-	for(i = 2; i <= N; i++) {
+	
+	dp[0][0] = 1;
+	for(i = 1; i <= N; i++) {
 		for(j = 0; j <= 9; j++) {
 			for(k = 0; k <= j; k++) {
 				dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
