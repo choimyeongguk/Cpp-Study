c20d5ea7efc143e6bb3cc32b3a23def7747a592a
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-26 10:43:31 +0900

    [Gold III] Title: ACM Craft, Time: 144 ms, Memory: 2828 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int N, K, X, Y, W, i, idx;
+	int D[1001], dist[1001] = { 0 };
+	int order[1001], degree[1001] = { 0 };
+	vector<vector<int>> graph;
+	queue<int> q;
+	
+	// 입력 
+	cin >> N >> K;
+	for(i = 1; i <= N; i++) {
+		cin >> D[i];
+	}
+	graph.resize(N + 1);
+	for(i = 0; i < K; i++) {
+		cin >> X >> Y;
+		graph[X].push_back(Y);
+	}
+	cin >> W;
+	
+	// 그래프 위상 정렬 
+	for(i = 1; i <= N; i++) {
+		for(auto& e : graph[i]) {
+			degree[e]++;
+		}
+	}
+	for(i = 1; i <= N; i++) {
+		if(!degree[i]) {
+			q.push(i);
+		}
+	}
+	for(i = 1; i <= N; i++) {
+		order[i] = q.front();
+		for(auto& e : graph[q.front()]) {
+			degree[e]--;
+			if(!degree[e]) {
+				q.push(e);
+			}
+		}
+		q.pop();
+	}
+	
+	// DAG
+	for(i = 1; i <= N; i++) {
+		idx = order[i];
+		dist[idx] += D[idx];
+		for(auto& e : graph[idx]) {
+			dist[e] = max(dist[e], dist[idx]);
+		}
+	}
+	cout << dist[W] << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int T;
+	cin >> T;
+	while(T--)
+		solve();
+	
+	return 0;
+}
