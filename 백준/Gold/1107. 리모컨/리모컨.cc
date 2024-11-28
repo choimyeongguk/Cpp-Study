6da5fd69980a87d7fa7b92f85dfb84cfd1ac68c1
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-28 16:49:50 +0900

    [Gold V] Title: 리모컨, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool broken[10] = { 0 };
+
+int digit(int x) {
+	int ret = 0;
+	do {
+		ret++;
+		x /= 10;
+	} while(x);
+	return ret;
+}
+
+bool canMake(int x) {
+	do {
+		if(broken[x % 10])
+			return false;
+		x /= 10;
+	} while(x);
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, M, ans, tmp, i;
+	
+	cin >> N >> M;
+	for(i = 0; i < M; i++) {
+		cin >> tmp;
+		broken[tmp] = true;
+	}
+	
+	ans = abs(N - 100);
+	for(i = N; i >= 0; i--) {
+		if(canMake(i)) {
+			ans = min(ans, digit(i) + N - i);
+		}
+	}
+	for(i = N; digit(i) + i - N < ans; i++) {
+		if(canMake(i)) {
+			ans = min(ans, digit(i) + i - N);
+		}
+	}
+	cout << ans;
+	
+	return 0;
+}
