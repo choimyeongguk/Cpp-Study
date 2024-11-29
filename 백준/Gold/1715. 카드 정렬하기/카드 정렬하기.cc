19f3fb85d4bff9c7ae3ea4228b914e23fe1e308e
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-11-29 11:45:36 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N, num, tmp, ans = 0, i;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	cin >> N;
+	for(i = 0; i < N; i++) {
+		cin >> num;
+		pq.push(num);
+	}
+	while(pq.size() > 1) {
+		tmp = pq.top();
+		pq.pop();
+		tmp += pq.top();
+		pq.pop();
+		ans += tmp;
+		pq.push(tmp);
+	}
+	cout << ans;
+	
+	return 0;
+}
