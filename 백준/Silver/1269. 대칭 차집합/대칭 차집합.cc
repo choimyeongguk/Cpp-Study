cc994b050f98e0ecc31cbe490329b35116166f50
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-12-03 11:38:05 +0900

    [Silver IV] Title: 대칭 차집합, Time: 132 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool used[100000001] = { false };
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	
-	int A, B, ans = 0, i;
+	int A, B, i;
+	unordered_set<int> used;
 	
 	cin >> A >> B;
 	A += B;
 	for(i = 0; i < A; i++) {
 		cin >> B;
-		used[B] = !used[B];
+		if(used.find(B) != used.end()) {
+			used.erase(B);
+		}
+		else {
+			used.insert(B);
+		}
 	}
-	for(i = 1; i <= 100000000; i++) {
-		if(used[i])
-			ans++;
-	}
-	cout << ans;
+	cout << used.size();
 	
 	return 0;
 }
