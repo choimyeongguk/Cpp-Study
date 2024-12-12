3eba714e0e8dd4be3913b00fc921a794486ed339
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-12-12 22:58:26 +0900

    [Gold III] Title: 소수의 연속합, Time: 32 ms, Memory: 9128 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool sieve[4000001] = { false };
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N, ans, sum, l, r, i, j;
+	vector<int> prime;
+	
+	for(i = 2; i <= 4000000; i++) {
+		if(!sieve[i]) {
+			for(j = 2; i * j <= 4000000; j++) {
+				sieve[j * i] = true;
+			}
+			prime.push_back(i);
+		}
+	}
+	
+	cin >> N;
+	ans = 0, sum = 2, l = 0, r = 0;
+	while(l <= r && r < prime.size() && prime[r] <= N) {
+		if(sum < N) {
+			sum += prime[++r];
+		}
+		else if(sum > N) {
+			sum -= prime[l++];
+		}
+		else {
+			ans++;
+			sum -= prime[l++];
+		}
+	}
+	cout << ans;
+	
+	return 0;
+}
