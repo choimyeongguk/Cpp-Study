d720e7ae86ca96a2620b88df11ddecb7b5edfa0d
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2024-12-13 11:32:34 +0900

    [Gold III] Title: 두 배열의 합, Time: 480 ms, Memory: 57728 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int T, n, m, sum, i, j;
+	long long ans;
+	int A[1001], B[1001];
+	map<int,long long> sumA, sumB;
+	
+	cin >> T >> n;
+	for(i = 0; i < n; i++)
+		cin >> A[i];
+	cin >> m;
+	for(i = 0; i < m; i++)
+		cin >> B[i];
+		
+	for(i = 0; i < n; i++) {
+		for(sum = 0, j = i; j < n; j++) {
+			sum += A[j];
+			sumA[sum]++;
+		}
+	}
+	for(i = 0; i < m; i++) {
+		for(sum = 0, j = i; j < m; j++) {
+			sum += B[j];
+			sumB[sum]++;
+		}
+	}
+	
+	ans = 0;
+	for(auto& e : sumA) {
+		auto it = sumB.find(T - e.first);
+		if(it != sumB.end()) {
+			ans += e.second * it->second;
+		}
+	}
+	cout << ans;
+	
+	return 0;
+}
