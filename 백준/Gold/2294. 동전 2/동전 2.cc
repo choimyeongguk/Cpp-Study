8fad211324594aef768369771b1ded5fc7341141
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2025-01-03 22:26:28 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -8,19 +8,15 @@ int main() {
 	cout.tie(0);
 	
 	int n, k, coin, i, j;
-	int dp[20001];
-	// dp[i] = j : i원을 만드는 데 필요한 최소 동전 수 j 
+	int dp[10001] = { 0 };
 	
 	cin >> n >> k;
-	dp[0] = 0;
 	for(i = 1; i <= k; i++) dp[i] = INF;
 	for(i = 0; i < n; i++) {
 		cin >> coin;
 		if(coin > k) continue;
-		for(j = 0; j + coin <= k; j++) {
-			if(dp[j] == INF) continue;
+		for(j = 0; j + coin <= k; j++)
 			dp[j + coin] = min(dp[j + coin], dp[j] + 1);
-		}
 	}
 	cout << (dp[k] == INF ? -1 : dp[k]);
 	
