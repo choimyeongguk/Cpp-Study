210026685545b530755e60c57dc05502511b04ff
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2025-01-03 22:24:43 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
 using namespace std;
 
 int main() {
@@ -12,21 +13,16 @@ int main() {
 	
 	cin >> n >> k;
 	dp[0] = 0;
-	for(i = 1; i <= k; i++) dp[i] = -1;
+	for(i = 1; i <= k; i++) dp[i] = INF;
 	for(i = 0; i < n; i++) {
 		cin >> coin;
 		if(coin > k) continue;
 		for(j = 0; j + coin <= k; j++) {
-			if(dp[j] == -1) continue;
-			if(dp[j + coin] == -1) {
-				dp[j + coin] = dp[j] + 1;
-			}
-			else {
-				dp[j + coin] = min(dp[j + coin], dp[j] + 1);
-			}
+			if(dp[j] == INF) continue;
+			dp[j + coin] = min(dp[j + coin], dp[j] + 1);
 		}
 	}
-	cout << dp[k];
+	cout << (dp[k] == INF ? -1 : dp[k]);
 	
 	return 0;
 }
