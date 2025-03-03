730c9fcbdbaf52e52c8b9777c1fdbb1f52cc92c6
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2025-03-03 16:23:26 +0900

    [Gold II] Title: 문제집, Time: 32 ms, Memory: 3836 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -6,38 +6,31 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 	
-	int N, M, A, B, cnt, num, min, i;
+	int N, M, A, B, num, i;
 	int in_degree[32001] = { 0 };
-	bool solved[32001] = { false };
 	vector<vector<int>> graph;
+	priority_queue<int, vector<int>, greater<int>> pq;
 	
-	cin >> N >> M;;
+	cin >> N >> M;
 	graph.resize(N + 1);
 	for(i = 0; i < M; i++) {
 		cin >> A >> B;
 		graph[A].push_back(B);
 		in_degree[B]++;
 	}
-
-	cnt = 0, num = 1;
-	while(cnt < N) {
-		if(solved[num]) {	// 이미 푼 문제 
-			num++;
+	
+	for(i = 1; i <= N; i++) {
+		if(!in_degree[i]) pq.push(i);
+	}
+	
+	while(!pq.empty()) {
+		num = pq.top();
+		pq.pop();
+		cout << num << " ";
+		for(auto& e : graph[num]) {
+			in_degree[e]--;
+			if(!in_degree[e]) pq.push(e);
 		}
-		else if(!in_degree[num]) {	// 풀 수 있는 문제 
-			cout << num << " ";
-			solved[num] = true;
-			min = num + 1;
-			cnt++;
-			for(auto& e : graph[num]) {
-				in_degree[e]--;
-				if(!in_degree[e]) {
-					min = min < e ? min : e;
-				}
-			}
-			num = min;
-		}
-		else num++;		// 풀 수 없는 문제 
 	}
 	
 	return 0;
