416566b5427e9e50674b9a09992701f85668189d
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2025-03-07 15:10:09 +0900

    [Gold II] Title: 공항, Time: 12 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.cc
+++ 백준/Gold/10775. 공항/공항.cc
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int tree[100001];
+
+int pa(int x) {
+	if(x == tree[x]) return x;
+	return tree[x] = pa(tree[x]);
+}
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 	
-	int G, P, g, ans = 0, i, j;
-	vector<int> dock;
+	int G, P, g, ans = 0, i;
 	
 	cin >> G >> P;
-	for(i = 1; i <= G; i++)
-		dock.emplace_back(i);
+	for(i = 1; i <= G; i++) tree[i] = i;
 	for(i = 0; i < P; i++) {
 		cin >> g;
-		auto it = upper_bound(dock.begin(), dock.end(), g);
-		if(it == dock.begin()) break;
-		else {
-			dock.erase(it - 1);
-			ans++;
-		}
+		g = pa(g);
+		// 게이트 사용 전에는 모두 자신의 부모는 자신의 번호
+		// 하지만 사용하면 책임을 자신의 전 번호 게이트에 떠넘김
+		// 결국 1번 게이트마저 책임을 떠넘겨 1번 게이트의 부모가 0이 되면 종료 
+		if(!tree[g]) break;
+		else tree[g]--, ans++;
 	}
 	cout << ans;
 	
