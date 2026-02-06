0be70b29ff4b9f9bdea47c894701f6f763737bbe
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-02-06 16:44:26 +0900

    [Silver II] Title: 88888, Time: 8 ms, Memory: 2924 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/33756. 88888/88888.cc
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vpll = vector<pll>;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+void setup() {
+    if(!ndebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    else {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+}
+
+void preprocess() {
+    ll i, j, k;
+
+}
+
+struct FastScanner {
+    static const int SZ = 1 << 20;
+    int idx, size;
+    char buf[SZ];
+    FastScanner(): idx(0), size(0) {}
+    char read() {
+        if (idx >= size) {
+            size = (int)fread(buf, 1, SZ, stdin);
+            idx = 0;
+            if (size == 0) return 0;
+        }
+        return buf[idx++];
+    }
+    ll nextInt() {
+        char c;
+        do c = read(); while (c <= ' ' && c);
+        ll sgn = 1;
+        if (c == '-') { sgn = -1; c = read(); }
+        ll x = 0;
+        while (c > ' ') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        return x * sgn;
+    }
+};
+
+void solve(ll testcase){
+    ll i, j, k;
+    FastScanner fs;
+    ll N = fs.nextInt();
+    while (N--) {
+        ll num = fs.nextInt();
+        if (num%8) { cout << "No\n"; continue; }
+        num /= 8;
+        ll cur = 8, flag = true;
+        while (num) {
+            if (cur < num%10) { flag = false; break; }
+            cur = num % 10;
+            num /= 10;
+        }
+        cout << (flag ? "Yes\n" : "No\n");
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+    ll t = 1;
+    // cin >> t;
+    for (ll testcase = 1; testcase <= t; testcase++){
+        solve(testcase);
+    }
+    return 0;
+}
