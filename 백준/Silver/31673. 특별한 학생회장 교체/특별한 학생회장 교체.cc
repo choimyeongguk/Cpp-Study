11555e9fbea6ebd0d5c5aca2b0fb92536c5d65b9
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-02-12 15:49:09 +0900

    [Silver III] Title: 특별한 학생회장 교체, Time: 8 ms, Memory: 4596 KB -BaekjoonHub

---
+++ 백준/Silver/31673. 특별한 학생회장 교체/특별한 학생회장 교체.cc
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vpll = vector<pll>;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+void setup() {
+    if(!ndebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    else {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+}
+
+void preprocess() {
+    ll i, j, k;
+
+}
+
+struct FastScanner {
+    static const int SZ = 1 << 20;
+    int idx, size;
+    char buf[SZ];
+    FastScanner(): idx(0), size(0) {}
+    char read() {
+        if (idx >= size) {
+            size = (int)fread(buf, 1, SZ, stdin);
+            idx = 0;
+            if (size == 0) return 0;
+        }
+        return buf[idx++];
+    }
+    ll nextInt() {
+        char c;
+        do c = read(); while (c <= ' ' && c);
+        ll sgn = 1;
+        if (c == '-') { sgn = -1; c = read(); }
+        ll x = 0;
+        while (c > ' ') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        return x * sgn;
+    }
+};
+
+void solve(ll testcase){
+    ll i, j, k;
+    FastScanner fs;
+    ll N=fs.nextInt(), M=fs.nextInt();
+    ll total = 0;
+    priority_queue<ll> pq;
+    for (i=0; i<N; i++) {
+        ll num = fs.nextInt();
+        pq.emplace(num);
+        total += num;
+    }
+    ll sum = 0;
+    for (i=0; i<N; i++) {
+        sum += pq.top(); pq.pop();
+        if (2*sum >= total) {
+            cout << M/(i+2);
+            return;
+        }
+    }
+}
+
+int main() {
+    setup();
+    preprocess();
+    ll t = 1;
+    // cin >> t;
+    for (ll testcase = 1; testcase <= t; testcase++){
+        solve(testcase);
+    }
+    return 0;
+}
