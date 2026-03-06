9c1f422f9b662049e8590abc4ae2da23465d2be7
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-03-06 17:45:36 +0900

    [Gold I] Title: 가장 작은 수, Time: 24 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Gold/26601. 가장 작은 수/가장 작은 수.cc
+++ 백준/Gold/26601. 가장 작은 수/가장 작은 수.cc
@@ -39,7 +39,7 @@ void preprocess() {
 void solve(ll testcase){
     ll i, j, k;
     const ll MOD = 2'000'003;
-    const ll MAX = 1'457'031;   // 1'453'125 ~ 1'460'937
+    const ll MAX = 1'456'054;   // 1'455'078 ~ 1'457'031
     ll N; cin >> N;
     priority_queue<ll,vl,greater<>> pq;  // [곱해야하는값,소수]
     vl isPrime(MAX+1, true);
