b96ffe6877f80a23aadb9df1f39d04502eceb01b
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-03-06 17:34:57 +0900

    [Gold I] Title: 가장 작은 수, Time: 32 ms, Memory: 20912 KB -BaekjoonHub

---
+++ 백준/Gold/26601. 가장 작은 수/가장 작은 수.cc
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vpll = vector<pll>;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+void setup() {
+    if(!ndebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    else {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+}
+
+void preprocess() {
+    ll i, j, k;
+
+}
+
+void solve(ll testcase){
+    ll i, j, k;
+    const ll MOD = 2'000'003;
+    const ll MAX = 2'000'000;
+    ll N; cin >> N;
+    priority_queue<ll,vl,greater<>> pq;  // [곱해야하는값,소수]
+    vl isPrime(MAX+1, true);
+    for (i=2; i<=MAX; i++) {
+        if (!isPrime[i]) continue;
+        pq.emplace(i);
+        for (j=i*i; j<=MAX; j+=i) isPrime[j] = false;
+    }
+    // cout << pq.size() << "\n";
+    ll ans = 1;
+    for (i=0; i<N; i++) {
+        auto Min = pq.top(); pq.pop();
+        ans = ans*Min %MOD;
+        pq.emplace(Min*Min);
+    }
+    cout << ans;
+}
+
+int main() {
+    setup();
+    preprocess();
+    ll t = 1;
+    // cin >> t;
+    for (ll testcase = 1; testcase <= t; testcase++){
+        solve(testcase);
+    }
+    return 0;
+}
