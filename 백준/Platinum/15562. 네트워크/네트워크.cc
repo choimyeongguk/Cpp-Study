756d1ffbdf78b000e4d53ff9d99515861b7ae2a4
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-03-07 16:18:10 +0900

    [Platinum V] Title: 네트워크, Time: 48 ms, Memory: 13560 KB -BaekjoonHub

--- 백준/Platinum/15562. 네트워크/네트워크.cc
+++ 백준/Platinum/15562. 네트워크/네트워크.cc
@@ -37,7 +37,7 @@ void preprocess() {
 }
 
 struct FastScanner {
-    static const int SZ = 1 << 21;
+    static const int SZ = 1 << 22;
     int idx, size;
     char buf[SZ];
     FastScanner(): idx(0), size(0) {}
