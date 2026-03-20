8f20b078d2466d5aa9b54d23dd7e2ac49f686b90
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-03-20 21:40:00 +0900

    [Gold I] Title: 서로소 조합, Time: 72 ms, Memory: 29544 KB -BaekjoonHub

---
+++ 백준/Gold/33916. 서로소 조합/서로소 조합.cc
@@ -0,0 +1,240 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vpll = vector<pll>;
+using vb = vector<bool>;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#define debug(x) ((void)0)
+#else
+constexpr bool ndebug = false;
+#define debug(x) io << #x << " = " << (x) << '\n'
+#endif
+
+struct FastIO {
+    static constexpr int SZ = 1 << 20;
+    int idxW = 0, idxR = 0, szR = 0;
+    char bufR[SZ], bufW[SZ];
+    int read() {
+        if (idxR >= szR) {
+            szR = (int)fread(bufR, 1, SZ, stdin);
+            idxR = 0;
+            if (szR == 0) return -1;
+        }
+        return bufR[idxR++];
+    }
+    ll getLL() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        bool neg = false;
+        if (c == '-') { neg = true; c = read(); }
+        ll x = 0;
+        while ('0' <= c && c <= '9') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        return neg ? -x : x;
+    }
+    ld getLD() {
+        int c; do c = read(); while (c <= ' ' && c != -1);
+        bool neg = false;
+        if (c == '-') {
+            neg = true;
+            c = read();
+        }
+        ld x = 0;
+        while ('0' <= c && c <= '9') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            ld base = 0.1L;
+            c = read();
+            while ('0' <= c && c <= '9') {
+                x += (c - '0') * base;
+                base *= 0.1L;
+                c = read();
+            }
+        }
+        return neg ? -x : x;
+    }
+    char getChar() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        return c == -1 ? 0 : (char)c;
+    }
+    string getStr() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        string str;
+        while (c > ' ') {
+            str.push_back((char)c);
+            c = read();
+        }
+        return str;
+    }
+    string getLine() {
+        int c = read();
+        while (c == '\r') c = read();
+        string s;
+        while (c != '\n' && c != -1) {
+            if (c != '\r') s.push_back((char)c);
+            c = read();
+        }
+        return s;
+    }
+    void flush() {
+        if (idxW) {
+            fwrite(bufW, 1, idxW, stdout);
+            idxW = 0;
+        }
+    }
+    FastIO& putLL(ll x) {
+        if (x == 0) { putChar('0'); return *this; }
+        unsigned long long y;
+        if (x < 0) putChar('-'), y = (unsigned long long)-(x+1)+1;
+        else y = (unsigned long long)x;
+        char s[24];
+        int n = 0;
+        while (y) {
+            s[n++] = char('0' + y%10);
+            y /= 10;
+        }
+        while (n--) putChar(s[n]);
+        return *this;
+    }
+    FastIO& putLD(ld x, int precision = 12) {
+        if (x < 0) {
+            putChar('-');
+            x = -x;
+        }
+        ld p10 = 1;
+        for (int i = 0; i < precision; i++) p10 *= 10.0L;
+        x += 0.5L / p10;
+        ll intPart = (ll)x;
+        putLL(intPart);
+        if (precision == 0) return *this;
+        x -= intPart;
+        putChar('.');
+        for (int i = 0; i < precision; i++) {
+            x *= 10;
+            int d = (int)x;
+            putChar(char('0' + d));
+            x -= d;
+        }
+        return *this;
+    }
+    FastIO& putChar(char c) {
+        if (idxW >= SZ) flush();
+        bufW[idxW++] = c;
+        return *this;
+    }
+    FastIO& putStr(const string& s) {
+        int n = (int)s.size();
+        int p = 0;
+        while (p < n) {
+            if (idxW >= SZ) flush();
+            int space = SZ - idxW;
+            int take = min(space, n - p);
+            memcpy(bufW + idxW, s.data() + p, take);
+            idxW += take;
+            p += take;
+        }
+        return *this;
+    }
+    FastIO& putStr(const char* s) {
+        int n = (int)strlen(s);
+        int p = 0;
+        while (p < n) {
+            if (idxW >= SZ) flush();
+            int space = SZ - idxW;
+            int take = min(space, n - p);
+            memcpy(bufW + idxW, s + p, take);
+            idxW += take;
+            p += take;
+        }
+        return *this;
+    }
+    FastIO& operator<<(ll x) { return putLL(x); }
+    FastIO& operator<<(ld x) { return putLD(x); }
+    FastIO& operator<<(char c) { return putChar(c); }
+    FastIO& operator<<(const string& s) { return putStr(s); }
+    FastIO& operator<<(const char* s) { return putStr(s); }
+    FastIO& operator>>(ll& x) { x = getLL(); return *this; }
+    FastIO& operator>>(ld& x) { x = getLD(); return *this; }
+    FastIO& operator>>(char& c) { c = getChar(); return *this; }
+    FastIO& operator>>(string& s) { s = getStr(); return *this; }
+    ~FastIO() { flush(); }
+};
+FastIO io;
+
+const ll MAX = 5'000;
+vl prime, spf(MAX+1, 0);
+
+void linear_sieve() {
+    ll i, j, k;
+    ll idx = 0;
+    for (i=2; i<spf.size(); i++) {
+        if (!spf[i]) {
+            spf[i] = i;
+            prime.emplace_back(i);
+        }
+        for (auto& p : prime) {
+            j = i * p;
+            if (j > MAX) break;
+            spf[j] = p; // 모든 합성수에 대해 spf는 딱 한번 결정됨 (x = spf * (x/spf)로 유일하게 결정되기 때문)
+            if (p == spf[i]) break;
+        }
+    }
+}
+
+vvl factor;
+// factor[i][j]=k := (i!에 들어있는 소수 idx2prime[j]의 개수 k)
+void preprocess() {
+    linear_sieve();
+    factor.assign(MAX+1, vl(prime.size(), 0));
+    for (ll i=2; i<=MAX; i++) {
+        for (ll j=0; j<prime.size(); j++) {
+            ll num = prime[j];
+            while (i/num > 0) {
+                factor[i][j] += i/num;
+                num *= prime[j];
+            }
+        }
+    }
+}
+
+void solve(ll testcase){
+    ll n1, r1, n2, r2; io >> n1 >> r1 >> n2 >> r2;
+    for (ll i=0; i<prime.size(); i++) {
+        ll pf1 = factor[n1][i] - factor[r1][i] - factor[n1-r1][i];
+        ll pf2 = factor[n2][i] - factor[r2][i] - factor[n2-r2][i];
+        if (pf1>0 && pf2>0) {
+            io << "0\n";
+            return;
+        }
+    }
+    io << "1\n";
+}
+
+int main() {
+    if(!ndebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    preprocess();
+    ll t = 1;
+    io >> t;
+    for (ll testcase = 1; testcase <= t; testcase++){
+        solve(testcase);
+    }
+    return 0;
+}
