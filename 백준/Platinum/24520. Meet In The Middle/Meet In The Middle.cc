ca9a351be96ea3a1a2cc73144b747e78aad040c1
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-03-21 12:51:56 +0900

    [Platinum III] Title: Meet In The Middle, Time: 88 ms, Memory: 25884 KB -BaekjoonHub

--- 백준/Platinum/24520. Meet In The Middle/Meet In The Middle.cc
+++ 백준/Platinum/24520. Meet In The Middle/Meet In The Middle.cc
@@ -20,7 +20,7 @@ constexpr bool ndebug = false;
 #endif
 
 struct FastIO {
-    static constexpr int SZ = 1 << 21;
+    static constexpr int SZ = 1 << 20;
     int idxW = 0, idxR = 0, szR = 0;
     char bufR[SZ], bufW[SZ];
     int read() {
