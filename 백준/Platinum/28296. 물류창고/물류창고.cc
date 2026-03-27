7973936f952c70ecad351338e40d19b5402ee9ae
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-03-27 18:38:03 +0900

    [Platinum III] Title: 물류창고, Time: 144 ms, Memory: 38056 KB -BaekjoonHub

---
+++ 백준/Platinum/28296. 물류창고/물류창고.cc
@@ -0,0 +1,234 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vpll = vector<pll>;
+using vb = vector<bool>;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#define debug(x) ((void)0)
+#else
+constexpr bool ndebug = false;
+#define debug(x) io << #x << " = " << (x) << '\n'
+#endif
+
+struct FastIO {
+    static constexpr int SZ = 1 << 20;
+    int idxW = 0, idxR = 0, szR = 0;
+    char bufR[SZ], bufW[SZ];
+    int read() {
+        if (idxR >= szR) {
+            szR = (int)fread(bufR, 1, SZ, stdin);
+            idxR = 0;
+            if (szR == 0) return -1;
+        }
+        return bufR[idxR++];
+    }
+    ll getLL() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        bool neg = false;
+        if (c == '-') { neg = true; c = read(); }
+        ll x = 0;
+        while ('0' <= c && c <= '9') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        return neg ? -x : x;
+    }
+    ld getLD() {
+        int c; do c = read(); while (c <= ' ' && c != -1);
+        bool neg = false;
+        if (c == '-') {
+            neg = true;
+            c = read();
+        }
+        ld x = 0;
+        while ('0' <= c && c <= '9') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            ld base = 0.1L;
+            c = read();
+            while ('0' <= c && c <= '9') {
+                x += (c - '0') * base;
+                base *= 0.1L;
+                c = read();
+            }
+        }
+        return neg ? -x : x;
+    }
+    char getChar() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        return c == -1 ? 0 : (char)c;
+    }
+    string getStr() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        string str;
+        while (c > ' ') {
+            str.push_back((char)c);
+            c = read();
+        }
+        return str;
+    }
+    string getLine() {
+        int c = read();
+        while (c == '\r') c = read();
+        string s;
+        while (c != '\n' && c != -1) {
+            if (c != '\r') s.push_back((char)c);
+            c = read();
+        }
+        return s;
+    }
+    void flush() {
+        if (idxW) {
+            fwrite(bufW, 1, idxW, stdout);
+            idxW = 0;
+        }
+    }
+    FastIO& putLL(ll x) {
+        if (x == 0) { putChar('0'); return *this; }
+        unsigned long long y;
+        if (x < 0) putChar('-'), y = (unsigned long long)-(x+1)+1;
+        else y = (unsigned long long)x;
+        char s[24];
+        int n = 0;
+        while (y) {
+            s[n++] = char('0' + y%10);
+            y /= 10;
+        }
+        while (n--) putChar(s[n]);
+        return *this;
+    }
+    FastIO& putLD(ld x, int precision = 12) {
+        if (x < 0) {
+            putChar('-');
+            x = -x;
+        }
+        ld p10 = 1;
+        for (int i = 0; i < precision; i++) p10 *= 10.0L;
+        x += 0.5L / p10;
+        ll intPart = (ll)x;
+        putLL(intPart);
+        if (precision == 0) return *this;
+        x -= intPart;
+        putChar('.');
+        for (int i = 0; i < precision; i++) {
+            x *= 10;
+            int d = (int)x;
+            putChar(char('0' + d));
+            x -= d;
+        }
+        return *this;
+    }
+    FastIO& putChar(char c) {
+        if (idxW >= SZ) flush();
+        bufW[idxW++] = c;
+        return *this;
+    }
+    FastIO& putStr(const string& s) {
+        int n = (int)s.size();
+        int p = 0;
+        while (p < n) {
+            if (idxW >= SZ) flush();
+            int space = SZ - idxW;
+            int take = min(space, n - p);
+            memcpy(bufW + idxW, s.data() + p, take);
+            idxW += take;
+            p += take;
+        }
+        return *this;
+    }
+    FastIO& putStr(const char* s) {
+        int n = (int)strlen(s);
+        int p = 0;
+        while (p < n) {
+            if (idxW >= SZ) flush();
+            int space = SZ - idxW;
+            int take = min(space, n - p);
+            memcpy(bufW + idxW, s + p, take);
+            idxW += take;
+            p += take;
+        }
+        return *this;
+    }
+    FastIO& operator<<(ll x) { return putLL(x); }
+    FastIO& operator<<(ld x) { return putLD(x); }
+    FastIO& operator<<(char c) { return putChar(c); }
+    FastIO& operator<<(const string& s) { return putStr(s); }
+    FastIO& operator<<(const char* s) { return putStr(s); }
+    FastIO& operator>>(ll& x) { x = getLL(); return *this; }
+    FastIO& operator>>(ld& x) { x = getLD(); return *this; }
+    FastIO& operator>>(char& c) { c = getChar(); return *this; }
+    FastIO& operator>>(string& s) { s = getStr(); return *this; }
+    ~FastIO() { flush(); }
+};
+FastIO io;
+
+void preprocess() {
+
+}
+
+struct Edge {
+    ll u, v, c;
+};
+
+void solve(ll testcase){
+    ll V, K, E; io >> V >> K >> E;
+    vector<unordered_map<ll,ll>> company(V+1);
+    // company[i][j]=k := i번 정점이 속해있는 그룹에서, j번 회사의 개수 k
+    for (ll i=1; i<=V; i++) company[i][io.getLL()] = 1;
+    vector<Edge> edges(E);
+    for (auto& [u,v,c]: edges) io >> u >> v >> c;
+    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+        return a.c > b.c;
+    });
+    vl ans(K+1), sz(V+1, 1), pa(V+1); iota(pa.begin(), pa.end(), 0);
+    function<ll(ll)> find = [&](ll x) {
+        if (pa[x] == x) return x;
+        return pa[x] = find(pa[x]);
+    };
+    function<void(ll,ll,ll)> merge = [&](ll x, ll y, ll c) {
+        x = find(x), y = find(y);
+        if (x == y) return;
+        if (sz[x] < sz[y]) swap(x, y);
+        for (auto [num,cnt]: company[y]) {
+            ans[num] += cnt * company[x][num] * c;
+            company[x][num] += cnt;
+        }
+        pa[y] = x;
+        sz[x] += sz[y];
+    };
+    for (auto [u,v,c]: edges) {
+        if (find(u) == find(v)) continue;
+        merge(u, v, c);
+    }
+    for (ll i=1; i<=K; i++) {
+        io << ans[i] << "\n";
+    }
+}
+
+int main() {
+    if(!ndebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    preprocess();
+    ll t = 1;
+    // io >> t;
+    for (ll testcase = 1; testcase <= t; testcase++){
+        solve(testcase);
+    }
+    return 0;
+}
