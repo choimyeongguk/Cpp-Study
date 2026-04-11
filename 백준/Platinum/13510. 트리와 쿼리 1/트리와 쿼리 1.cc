30f53d322e255bf5b4988b947dc6a650c7742dc7
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-04-11 18:34:53 +0900

    [Platinum I] Title: 트리와 쿼리 1, Time: 96 ms, Memory: 30472 KB -BaekjoonHub

---
+++ 백준/Platinum/13510. 트리와 쿼리 1/트리와 쿼리 1.cc
@@ -0,0 +1,300 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vpll = vector<pll>;
+using vb = vector<bool>;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#define debug(x) ((void)0)
+#else
+constexpr bool ndebug = false;
+#define debug(x) io << #x << " = " << (x) << '\n'
+#endif
+
+struct FastIO {
+    static constexpr int SZ = 1 << 20;
+    int idxW = 0, idxR = 0, szR = 0;
+    char bufR[SZ], bufW[SZ];
+    int read() {
+        if (idxR >= szR) {
+            szR = (int)fread(bufR, 1, SZ, stdin);
+            idxR = 0;
+            if (szR == 0) return -1;
+        }
+        return bufR[idxR++];
+    }
+    ll getLL() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        bool neg = false;
+        if (c == '-') { neg = true; c = read(); }
+        ll x = 0;
+        while ('0' <= c && c <= '9') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        return neg ? -x : x;
+    }
+    ld getLD() {
+        int c; do c = read(); while (c <= ' ' && c != -1);
+        bool neg = false;
+        if (c == '-') {
+            neg = true;
+            c = read();
+        }
+        ld x = 0;
+        while ('0' <= c && c <= '9') {
+            x = x * 10 + (c - '0');
+            c = read();
+        }
+        if (c == '.') {
+            ld base = 0.1L;
+            c = read();
+            while ('0' <= c && c <= '9') {
+                x += (c - '0') * base;
+                base *= 0.1L;
+                c = read();
+            }
+        }
+        return neg ? -x : x;
+    }
+    char getChar() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        return c == -1 ? 0 : (char)c;
+    }
+    string getStr() {
+        int c;
+        do c = read(); while (c <= ' ' && c != -1);
+        string str;
+        while (c > ' ') {
+            str.push_back((char)c);
+            c = read();
+        }
+        return str;
+    }
+    string getLine() {
+        int c = read();
+        while (c == '\r') c = read();
+        string s;
+        while (c != '\n' && c != -1) {
+            if (c != '\r') s.push_back((char)c);
+            c = read();
+        }
+        return s;
+    }
+    void flush() {
+        if (idxW) {
+            fwrite(bufW, 1, idxW, stdout);
+            idxW = 0;
+        }
+    }
+    FastIO& putLL(ll x) {
+        if (x == 0) { putChar('0'); return *this; }
+        unsigned long long y;
+        if (x < 0) putChar('-'), y = (unsigned long long)-(x+1)+1;
+        else y = (unsigned long long)x;
+        char s[24];
+        int n = 0;
+        while (y) {
+            s[n++] = char('0' + y%10);
+            y /= 10;
+        }
+        while (n--) putChar(s[n]);
+        return *this;
+    }
+    FastIO& putLD(ld x, int precision = 12) {
+        if (x < 0) {
+            putChar('-');
+            x = -x;
+        }
+        ld p10 = 1;
+        for (int i = 0; i < precision; i++) p10 *= 10.0L;
+        x += 0.5L / p10;
+        ll intPart = (ll)x;
+        putLL(intPart);
+        if (precision == 0) return *this;
+        x -= intPart;
+        putChar('.');
+        for (int i = 0; i < precision; i++) {
+            x *= 10;
+            int d = (int)x;
+            putChar(char('0' + d));
+            x -= d;
+        }
+        return *this;
+    }
+    FastIO& putChar(char c) {
+        if (idxW >= SZ) flush();
+        bufW[idxW++] = c;
+        return *this;
+    }
+    FastIO& putStr(const string& s) {
+        int n = (int)s.size();
+        int p = 0;
+        while (p < n) {
+            if (idxW >= SZ) flush();
+            int space = SZ - idxW;
+            int take = min(space, n - p);
+            memcpy(bufW + idxW, s.data() + p, take);
+            idxW += take;
+            p += take;
+        }
+        return *this;
+    }
+    FastIO& putStr(const char* s) {
+        int n = (int)strlen(s);
+        int p = 0;
+        while (p < n) {
+            if (idxW >= SZ) flush();
+            int space = SZ - idxW;
+            int take = min(space, n - p);
+            memcpy(bufW + idxW, s + p, take);
+            idxW += take;
+            p += take;
+        }
+        return *this;
+    }
+    FastIO& operator<<(ll x) { return putLL(x); }
+    FastIO& operator<<(ld x) { return putLD(x); }
+    FastIO& operator<<(char c) { return putChar(c); }
+    FastIO& operator<<(const string& s) { return putStr(s); }
+    FastIO& operator<<(const char* s) { return putStr(s); }
+    FastIO& operator>>(ll& x) { x = getLL(); return *this; }
+    FastIO& operator>>(ld& x) { x = getLD(); return *this; }
+    FastIO& operator>>(char& c) { c = getChar(); return *this; }
+    FastIO& operator>>(string& s) { s = getStr(); return *this; }
+    ~FastIO() { flush(); }
+};
+FastIO io;
+
+void preprocess() {
+
+}
+
+struct SegTree {
+    ll n; vl tree;
+    SegTree(vl& arr): n(arr.size()), tree(n<<2) {
+        function<void(ll,ll,ll)> init = [&](ll i, ll s, ll e) {
+            if (s == e) { tree[i] = arr[s]; return; }
+            ll m = (s+e)>>1;
+            init(i<<1, s, m);
+            init(i<<1|1, m+1, e);
+            tree[i] = max(tree[i<<1], tree[i<<1|1]);
+        };
+        init(1, 0, n-1);
+    }
+    void update(ll pos, ll val) { update(1, 0, n-1, pos, val); }
+    void update(ll i, ll s, ll e, ll pos, ll val) {
+        if (s == e) { tree[i] = val; return; }
+        ll m = (s+e)>>1;
+        pos<=m ? update(i<<1, s, m, pos, val) : update(i<<1|1, m+1, e, pos, val);
+        tree[i] = max(tree[i<<1], tree[i<<1|1]);
+    }
+    ll query(ll l, ll r) { return query(1, 0, n-1, l, r); }
+    ll query(ll i, ll s, ll e, ll l, ll r) {
+        if (s>r || e<l) return -1;
+        if (l<=s && e<=r) return tree[i];
+        ll m = (s+e)>>1;
+        return max(query(i<<1, s, m, l, r), query(i<<1|1, m+1, e, l, r));
+    }
+};
+
+struct Edge {
+    ll u, v, w;
+};
+
+void solve(ll testcase){
+    // Input Tree
+    ll N; io >> N;
+    vvl G(N+1);
+    vector<Edge> edges(N-1);
+    for (auto& [u,v,w]: edges) {
+        io >> u >> v >> w;
+        G[u].emplace_back(v);
+        G[v].emplace_back(u);
+    }
+
+    // HLD
+    ll time = 0; vl depth(N+1), sz(N+1), pa(N+1), top(N+1), in(N+1), out(N+1);
+    function<void(ll,ll)> dfs1 = [&](ll cur, ll prev) {
+        sz[cur] = 1, pa[cur] = prev;
+        ll heavy = -1, mx = -1;
+        for (ll i=0; i<G[cur].size(); i++) {
+            ll nxt = G[cur][i]; if (nxt == prev) continue;
+            depth[nxt] = depth[cur] + 1;
+            dfs1(nxt, cur);
+            sz[cur] += sz[nxt];
+            if (mx < sz[nxt]) mx = sz[nxt], heavy = i;
+        }
+        if (heavy != -1) swap(G[cur][0], G[cur][heavy]);
+    };
+    function<void(ll,ll,ll)> dfs2 = [&](ll cur, ll prev, ll head) {
+        top[cur] = head, in[cur] = time++;
+        bool heavy = true;
+        for (auto nxt: G[cur]) {
+            if (nxt == prev) continue;
+            dfs2(nxt, cur, heavy ? head : nxt);
+            heavy = false;
+        }
+        out[cur] = time-1;
+    };
+    dfs1(1, -1);
+    dfs2(1, -1, 1);
+
+    // Query
+    vl arr(N);
+    for (auto& [u,v,w]: edges) {
+        if (pa[v] == u) swap(u, v); // first가 자식이 되도록
+        arr[in[u]] = w;
+    }
+    SegTree seg(arr);
+    function<void(ll,ll)> update = [&](ll pos, ll val) {
+        seg.update(in[pos], val);
+    };
+    function<ll(ll,ll)> query = [&](ll u, ll v) {
+        ll ret = -1;
+        while (top[u] != top[v]) {
+            if (depth[top[u]] < depth[top[v]]) swap(u, v);
+            ret = max(ret, seg.query(in[top[u]], in[u]));
+            u = pa[top[u]];
+        }
+        if (depth[u] > depth[v]) swap(u, v);
+        ret = max(ret, seg.query(in[u]+1, in[v]));
+        return ret;
+    };
+    ll Q; io >> Q;
+    while (Q--) {
+        ll op, a, b; io >> op >> a >> b;
+        switch (op) {
+            case 1:
+                update(edges[a-1].u, b);
+                break;
+            case 2:
+                io << query(a, b) << '\n';
+                break;
+        }
+    }
+}
+
+int main() {
+    if(!ndebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    preprocess();
+    ll t = 1;
+    // io >> t;
+    for (ll testcase = 1; testcase <= t; testcase++){
+        solve(testcase);
+    }
+    return 0;
+}
