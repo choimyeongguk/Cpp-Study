fe291585b50008569d8749f3fe048b38f3f188e3
최명국 <100114511+choimyeongguk@users.noreply.github.com>
2026-04-14 20:33:07 +0900

    [Diamond III] Title: 트리와 쿼리 10, Time: 200 ms, Memory: 24640 KB -BaekjoonHub

--- 백준/Diamond/13519. 트리와 쿼리 10/트리와 쿼리 10.cc
+++ 백준/Diamond/13519. 트리와 쿼리 10/트리와 쿼리 10.cc
@@ -22,7 +22,7 @@ constexpr bool ndebug = false;
 #endif
 
 struct FastIO {
-    static constexpr int SZ = 1 << 20;
+    static constexpr int SZ = 1 << 21;
     int idxW = 0, idxR = 0, szR = 0;
     char bufR[SZ], bufW[SZ];
     int read() {
